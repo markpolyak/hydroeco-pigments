58b4956d0c9106b3d101454473148bcc46889191
Daniel Chuev <Daniel Chuev>
2016-09-29 17:44:06 +0300

    'fix'

---
+++ hydro-data/public_html/getStation.php
@@ -0,0 +1,18 @@
+<?php
+$cnn = pg_connect("host=pg.sweb.ru port=5432 dbname=mpolyakru_hbio user=mpolyakru_hbio password=test1234") or die("Connection Error");
+
+$area = $_GET["area"];
+
+if ($area)
+{    
+	$q = "SELECT station.name, station.id_station
+		  FROM station 
+		  WHERE station.id_water_area = '$area'";
+	$result = pg_query ($cnn, $q);
+	$rows = array();
+	while($r = pg_fetch_assoc($result)) {
+		$rows[] = $r;
+	}
+	print json_encode($rows);
+}
+?>
